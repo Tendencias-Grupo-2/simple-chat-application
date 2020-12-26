720d875283ede121599c57454df57c5cfdec9d3a
Jeremy Martínez <jeremy-alejandro_28@hotmail.com>
2020-12-26 14:58:16 -0400

    testing bad commit

--- simple-chat-front/src/App.js
+++ simple-chat-front/src/App.js
@@ -7,18 +7,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editas <code>src/App.js</code> and save to reload.
+          Editas code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
         </a>
       </header>
-    </div>
+    </div >
   );
 }
 
