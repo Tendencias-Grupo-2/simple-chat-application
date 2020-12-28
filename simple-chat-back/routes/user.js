630b6b21834e13908696a10206df22164ddffa5e
Lunaticfrg0 <emilio3scano@gmail.com>
2020-12-27 20:09:11 -0400

    Refactored code and deleted code not used

--- simple-chat-back/routes/user.js
+++ simple-chat-back/routes/user.js
@@ -10,7 +10,8 @@ router.get("/users", async (req, res) =>{
               return res.status(404).send()
            }
           res.send(user) 
-       }catch(error){
+       }
+       catch(error){
           res.status(500).send()
        }
   })
@@ -20,7 +21,8 @@ router.get("/users", async (req, res) =>{
     try{
         await user.save()
         res.status(201).send(user)
-    } catch(error){
+    } 
+    catch(error){
         res.status(400).send(error)
     }
   })
