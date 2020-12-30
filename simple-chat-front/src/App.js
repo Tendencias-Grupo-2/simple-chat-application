292bacec1aff8a764cf1b1956921421dc2bf98b2
Jeremy Martínez <jeremy-alejandro_28@hotmail.com>
2020-12-30 17:20:10 -0400

    deleted unused commented code

--- simple-chat-front/src/App.js
+++ simple-chat-front/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Chat from './components/Chat/Chat';
-// import TestComponent from './components/TestComponent/TestComponent'
 import UserNameInput from './components/UserNameInput/UserNameInput';
 
 function App() {
@@ -8,21 +7,6 @@ function App() {
     <div className="App">
       <UserNameInput />
       <Chat></Chat>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editass <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <TestComponent /> */}
     </div>
   );
 }
