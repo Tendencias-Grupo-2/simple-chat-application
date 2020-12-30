df52ee5417ad93eb906329b74a1ecf296aecbcde
Jeremy Martínez <jeremy-alejandro_28@hotmail.com>
2020-12-30 15:13:42 -0400

    initialized chat component and test file

---
+++ simple-chat-front/src/components/Chat/Chat.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Chat-Style.css'
+
+const Chat = () => {
+    return (
+        <div>
+            <h2>My testComponent</h2>
+        </div>
+    );
+}
+
+export default Chat;
