439fa415c7d7f7b0b391e35a870de66d60f5e7c4
Axcel Espinal <hyrulelink8zelda@gmail.com>
2020-12-31 13:14:25 -0400

    Formatting apiRequests.js

--- simple-chat-front/src/utils/apiRequests.js
+++ simple-chat-front/src/utils/apiRequests.js
@@ -1,10 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
-// Make a GET request with a shorthand method
-axios.get('https://api.github.com/users/hacktivist123');
+const getData = () => {
+  axios.get("https://api.github.com/users/hacktivist123");
+};
 
-// Make a Post Request with a shorthand method
-axios.post('/signup', {
-    firstName: 'Axcel',
-    lastName: 'Espinal'
-});
+export default getData;
