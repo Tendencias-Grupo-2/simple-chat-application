d2b4738e7f1bda49b4cc0e9047b49f0acd289e6b
Axcel Espinal <hyrulelink8zelda@gmail.com>
2021-01-02 17:05:47 -0400

    Update App.js

--- simple-chat-front/src/App.js
+++ simple-chat-front/src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Chat from "./components/Chat/Chat";
 import UserNameInput from "./components/UserNameInput/UserNameInput";
-import TestComponent from "./components/TestComponent/TestComponent";
 
 function App() {
   return (
     <div className="App">
-      <TestComponent />
       <UserNameInput />
       <Chat></Chat>
     </div>
