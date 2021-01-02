2a62c773c131dd7155cdeeda4ac3ef7a57d87310
Roniel Valdez <roniel_valdez@outlook.com>
2021-01-02 17:29:57 -0400

    add environmnent variable to backend deployment

--- simple-chat-back/db/mongoose.js
+++ simple-chat-back/db/mongoose.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-mongoose.connect(process.env.mongoDBConnectionString,{
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING,{
     useNewUrlParser:true,
     useCreateIndex:true,
     useFindAndModify: false,
