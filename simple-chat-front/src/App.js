f420ae91bd14311479fc6664d0a9cfaf4dcd1ff2
PdMendoza <pedromendozaagramonte@gmail.com>
2021-01-02 16:36:01 -0400

    Created the routing logic

--- simple-chat-front/src/App.js
+++ simple-chat-front/src/App.js
@@ -1,12 +1,24 @@
-import './App.css';
-import Chat from './components/Chat/Chat';
-import UserNameInput from './components/UserNameInput/UserNameInput';
+import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Chat from "./components/Chat/Chat";
+import UserNameInput from "./components/UserNameInput/UserNameInput";
 
 function App() {
   return (
     <div className="App">
-      <UserNameInput />
-      <Chat></Chat>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={UserNameInput}></Route>
+          <Route exact path="/chat" component={Chat}></Route>
+          <Redirect exact from="*" to="/"></Redirect>
+        </Switch>
+      </Router>
     </div>
   );
 }
