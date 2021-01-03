262c50045967c35947f1604d2b98a1b292d4f12a
Axcel Espinal <hyrulelink8zelda@gmail.com>
2021-01-03 12:22:01 -0400

    Update apiRequests.js

--- simple-chat-front/src/utils/apiRequests.js
+++ simple-chat-front/src/utils/apiRequests.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export default async function apiRequests() {
   try {
-    const data = await axios.get("https://api.github.com/users/AxcelEspinal");
+    const data = await axios.get(process.env.REACT_APP_API_LINK);
     return data.data;
   } catch (error) {
     console.log("error", error);
