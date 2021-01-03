c531376a407a6ce1673623fee361eb1e57801917
Axcel Espinal <hyrulelink8zelda@gmail.com>
2021-01-02 20:06:45 -0400

    Update TestComponent.jsx

--- simple-chat-front/src/components/TestComponent/TestComponent.jsx
+++ simple-chat-front/src/components/TestComponent/TestComponent.jsx
@@ -1,16 +1,9 @@
 import { React, useState, useEffect, Component } from "react";
 import "./TestComponent-Style.css";
-import apiRequests from "../../utils/apiRequests.js";
 
 const TestComponent = () => {
   const [value, setValue] = useState(0);
-  const [apiData, setApiData] = useState(0);
-
-  useEffect(() => {
-    apiRequests().then((apiData) => {
-      setApiData(apiData);
-    });
-  }, []);
+  
   return (
     <div>
       <h2>My testComponent</h2>
@@ -21,7 +14,6 @@ const TestComponent = () => {
       <button className="pressme-btnm" onClick={() => setValue(value - 1)}>
         Press Me
       </button>
-      <h4>Username: {apiData.login}</h4>
     </div>
   );
 };
