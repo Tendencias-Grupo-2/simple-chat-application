dfde9fe217544c3360281670ac8324a130a3e15b
Jeremy Martínez <jeremy-alejandro_28@hotmail.com>
2021-01-03 14:45:44 -0400

    added contact name placeholder

--- simple-chat-front/src/components/Chat/Chat.jsx
+++ simple-chat-front/src/components/Chat/Chat.jsx
@@ -33,7 +33,9 @@ let roomsMockData = [
 const Chat = () => {
     return (
         <div className='chat'>
-            <div className='chat__header'>Placeholder Text</div>
+            <div className='chat__header'>
+                <span className='chat__headertext'>Contact Name</span>
+            </div>
             <div className='chat__body'>
                 <div className='chat__left'>
                     <span className='chat__left--title'>Select a room from the list to start chatting.</span>
