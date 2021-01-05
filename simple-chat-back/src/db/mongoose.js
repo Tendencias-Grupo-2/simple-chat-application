bca528722d81ad28b8f18f9852996cb043c3b9cd
Lunaticfrg0 <emilio3scano@gmail.com>
2021-01-04 21:42:38 -0400

    Reverted dotenv dependencie

--- simple-chat-back/src/db/mongoose.js
+++ simple-chat-back/src/db/mongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config()
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING,{
     useNewUrlParser:true,
     useCreateIndex:true,
