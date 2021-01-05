863cfc87e104819ad6c98762ffac085a2c168359
Nelly <estrelladebelen2000@gmail.com>
2021-01-04 23:00:39 -0400

    Format correction

--- simple-chat-back/src/models/messageModel.js
+++ simple-chat-back/src/models/messageModel.js
@@ -1,23 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const messageSchema = new mongoose.Schema({
-
-	user:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-        trim: true
+const messageSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      trim: true
     },
-    text:{
-        type: String,
-        required: true,
-        trim: true
-        
+    text: {
+      type: String,
+      required: true,
+      trim: true
     }
-},
-    {timestamps: true}
-)
+  },
+  { timestamps: true }
+);
 
-const Message = mongoose.model('Message', messageSchema )
-module.exports = Message
-
+const Message = mongoose.model("Message", messageSchema);
+module.exports = Message;
