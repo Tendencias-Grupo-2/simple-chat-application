cee7d2671a2ea466a737d2294d41e51db2cd0725
Roniel Valdez <roniel_valdez@outlook.com>
2021-01-06 16:10:44 -0400

    add env to test on mongdb

--- simple-chat-back/src/db/mongoose.js
+++ simple-chat-back/src/db/mongoose.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
 dotenv.config()
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING,{
+
+let connectionString;
+if (process.env.NODE_ENV  === 'development') {
+  connectionString = process.env.MONGODB_CONNECTION_STRING_TEST; 
+} 
+else {
+  connectionString = process.env.MONGODB_CONNECTION_STRING; 
+}
+
+mongoose.connect(connectionString,{
     useNewUrlParser:true,
     useCreateIndex:true,
     useFindAndModify: false,
