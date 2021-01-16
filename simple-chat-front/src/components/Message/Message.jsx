873c1aea605c0361b9afb5b50fbe42e96e52712e
PdMendoza <pedromendozaagramonte@gmail.com>
2021-01-16 13:00:02 -0400

    Parsed the UNIX timestamp to a comprehensive format

--- simple-chat-front/src/components/Message/Message.jsx
+++ simple-chat-front/src/components/Message/Message.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import './Message-Style.css'
-
+import React from "react";
+import "./Message-Style.css";
+import PseudoDateParser from "@pseudomera/date-parser/dist/dateParser";
 
 const Message = ({ message, imSender, createdAt, userName }) => {
-    return (
-        <div className={`message ${imSender === true ? "message--me" : null}`}>
-            <span className='message__text'>{message}</span>
-            <span className='message__time'>{createdAt} 🕒</span>
-        </div>
-    );
-}
+  const parsedNumber = PseudoDateParser(createdAt);
+  return (
+    <div className={`message ${imSender === true ? "message--me" : null}`}>
+      <span className="message__text">{message}</span>
+      <span className="message__time">
+        {parsedNumber.day} {parsedNumber.monthShort} {parsedNumber.year}{" "}
+        {parsedNumber.hour}:{parsedNumber.minutes} {parsedNumber.amOrPm} 🕒
+      </span>
+    </div>
+  );
+};
 export default Message;
