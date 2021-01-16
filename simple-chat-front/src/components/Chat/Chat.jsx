8a16034307f36663b5d35f717fb50664822b2baf
Emilio Escaño <52083070+Lunaticfrg0@users.noreply.github.com>
2021-01-15 20:49:57 -0400

    Tpf 68 switch room event (#51)
    * Delete public directory
    
    * Delete models not used
    
    * Delete routers
    
    * Change index to avoid magic
    strings in the exitRoom event
    
    * Change chat.jsx to allow the room transfer
    
    * fixed code warnings and switchRoom logic issue
    
    Co-authored-by: Jeremy Martínez <jeremy-alejandro_28@hotmail.com>

--- simple-chat-front/src/components/Chat/Chat.jsx
+++ simple-chat-front/src/components/Chat/Chat.jsx
@@ -1,13 +1,13 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect } from "react";
 import io from "socket.io-client";
 import { useForm } from "react-hook-form";
-import Room from '../Room/Room';
-import Message from '../Message/Message';
+import Room from "../Room/Room";
+import Message from "../Message/Message";
 import { BiMailSend, BiExit } from "react-icons/bi";
-import './Chat-Style.css'
-import roomsMockData from "../../samples/roomsMockData.json"
+import "./Chat-Style.css";
+import roomsMockData from "../../samples/roomsMockData.json";
 import { userNameContext } from "../../utils/userNameContext";
-import { Redirect } from "react-router-dom"
+import { Redirect } from "react-router-dom";
 
 const HOST = process.env.REACT_APP_STAGING_HOST;
 const socket = io(HOST);
@@ -16,39 +16,46 @@ const Chat = () => {
     const [currentRoom, setCurrentRoom] = useState("");
     //TODO: Implement room active users list
     // const [users, setUsers] = useState('')
-    const [message, setMessage] = useState('')
-    const [infoMessages, setInfoMessages] = useState([])
-    const [messages, setMessages] = useState([])
-    const [errorFlag, setErrorFlag] = useState(0)
+    const [message, setMessage] = useState("");
+    const [infoMessages, setInfoMessages] = useState([]);
+    const [messages, setMessages] = useState([]);
+    const [errorFlag, setErrorFlag] = useState(0);
     const { contextName } = useContext(userNameContext);
     const { register, handleSubmit, reset } = useForm({});
 
     const sendMessage = (data) => {
         if (message) {
-            socket.emit('sendMessage', message, () => setMessage(''));
+            socket.emit("sendMessage", message, () => setMessage(""));
             data.message = reset();
         }
-    }
+    };
 
     const joinRoom = (roomId) => {
-        setMessages([])
-        socket.emit('join', { username: contextName, room: roomId }, (error) => {
+        setMessages([]);
+        socket.emit("join", { username: contextName, room: roomId }, (error) => {
             if (error) {
-                alert(error)
+                alert(error);
                 setErrorFlag(1);
             }
-        })
-    }
+        });
+    };
 
     const exitRoom = () => {
-        setMessages([])
-        setCurrentRoom("")
-        socket.emit('exitRoom', currentRoom);
-    }
+        setMessages([]);
+        setCurrentRoom("");
+        socket.emit("exitRoom", currentRoom);
+    };
+
+    const switchRoom = (currentRoom, newRoom) => {
+        socket.emit("exitRoom", currentRoom);
+        setMessages([]);
+        joinRoom(newRoom);
+        setCurrentRoom(newRoom);
+    };
 
     /* istanbul ignore next */
     useEffect(() => {
-        socket.on('message', message => {
+        socket.on("message", (message) => {
             if (message.username === "ChatApp") {
                 setInfoMessages((messages) => [...messages, message]);
             } else {
@@ -61,39 +68,43 @@ const Chat = () => {
         //     console.log(users)
         //     setUsers(users);
         // });
-
     }, [contextName]);
 
     if (errorFlag) {
-        return (
-            <Redirect to="/" />
-        )
+        return <Redirect to="/" />;
     }
 
     return (
-        <div className='chat'>
-            <div className='chat__header'>
+        <div className="chat">
+            <div className="chat__header">
                 {currentRoom !== "" ? (
-                    <span className='chat__headertext'>Contact Name</span>
+                    <span className="chat__headertext">Contact Name</span>
                 ) : null}
                 {currentRoom !== "" ? (
-                    <span className='chat__headertext chat__headertext--exit' onClick={exitRoom}><BiExit />Exit Room</span>
+                    <span
+                        className="chat__headertext chat__headertext--exit"
+                        onClick={exitRoom}
+                    >
+                        <BiExit />
+            Exit Room
+                    </span>
                 ) : null}
             </div>
-            <div className='chat__body'>
+            <div className="chat__body">
                 {currentRoom !== "" ? (
-                    <div className='chat__left'>
-                        <div className='chat__left--inner'>
+                    <div className="chat__left">
+                        <div className="chat__left--inner">
                             {infoMessages.map((message) => (
                                 <Message
                                     message={message.text + " has joined the chat"}
                                     createdAt={message.createdAt}
                                     userName={message.username}
                                     imSender={
-                                        contextName.toLowerCase() === message.username ? true : false
+                                        contextName.toLowerCase() === message.username
+                                            ? true
+                                            : false
                                     }
-                                >
-                                </Message>
+                                ></Message>
                             ))}
                             {messages.map((message) => (
                                 <Message
@@ -101,45 +112,62 @@ const Chat = () => {
                                     createdAt={message.createdAt}
                                     userName={message.username}
                                     imSender={
-                                        contextName.toLowerCase() === message.username ? true : false
+                                        contextName.toLowerCase() === message.username
+                                            ? true
+                                            : false
                                     }
-                                >
-                                </Message>
+                                ></Message>
                             ))}
                         </div>
-                        <div className='chat__bar'>
-                            <form action="" className='chat__barform' onSubmit={handleSubmit(sendMessage)}>
-                                <input className='chat__barinput' ref={register} name="message" type="text" onChange={(e) => {
-                                    setMessage(e.target.value)
-                                }} />
-                                <button className="chat__barbtn"><BiMailSend size="25" color="#FFF" /></button>
+                        <div className="chat__bar">
+                            <form
+                                action=""
+                                className="chat__barform"
+                                onSubmit={handleSubmit(sendMessage)}
+                            >
+                                <input
+                                    className="chat__barinput"
+                                    ref={register}
+                                    name="message"
+                                    type="text"
+                                    onChange={(e) => {
+                                        setMessage(e.target.value);
+                                    }}
+                                />
+                                <button className="chat__barbtn">
+                                    <BiMailSend size="25" color="#FFF" />
+                                </button>
                             </form>
                         </div>
                     </div>
-
-                ) :
-                    (
-                        <div className='chat__left'>
-                            <span className='chat__left--title'>Select a room from the list to start chatting.</span>
-                            <span className='chat__left--subtitle'>Simple Chat Application</span>
+                ) : (
+                        <div className="chat__left">
+                            <span className="chat__left--title">
+                                Select a room from the list to start chatting.
+            </span>
+                            <span className="chat__left--subtitle">
+                                Simple Chat Application
+            </span>
                         </div>
                     )}
-                <div className='chat__right'>
+                <div className="chat__right">
                     {roomsMockData.map((room) => (
                         <Room
                             key={room.RoomId}
                             isRoomSelected={room.RoomId === currentRoom}
                             roomId={room.RoomId}
                             onClick={() => {
-                                setCurrentRoom(room.RoomId)
-                                joinRoom(room.RoomId)
-                            }}>
-                        </Room>
+                                setCurrentRoom(room.RoomId);
+                                currentRoom === ""
+                                    ? joinRoom(room.RoomId)
+                                    : switchRoom(currentRoom, room.RoomId);
+                            }}
+                        ></Room>
                     ))}
                 </div>
-            </div >
-        </div >
+            </div>
+        </div>
     );
-}
+};
 
 export default Chat;
